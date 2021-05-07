af447a6d9feb0a5a041192ad871adbf6f3f25df5
João Sobral <joca_sobral@hotmail.com>
2021-05-07 14:33:46 +0100

    Create Pedido.java

---
+++ ProjetoRestaurante/src/projetorestaurante/Pedido.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetorestaurante;
+import java.util.Date;
+import java.util.ArrayList;
+/**
+ *
+ * @author User
+ */
+public class Pedido {
+    
+    private Date dataAbertura;
+    private Date dataFecho;
+    private int estadoPedido;
+    
+    
+
+    public Pedido(Date dataAbertura, Date dataFecho, int estadoPedido) {
+        
+    }
+    
+    
+    
+    
+}
